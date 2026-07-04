c4c5ac876f17bc4ab40eda926f5e5aaa7f04e29e
Alan Huang <cc.alan@gmail.com>
2026-07-04 21:17:10 +1000

    Add src/torlink/magnet.js

---
+++ src/torlink/magnet.js
@@ -0,0 +1,29 @@
+const TRACKERS = [
+  "udp://tracker.opentrackr.org:1337/announce",
+  "udp://open.demonii.com:1337/announce",
+  "udp://tracker.openbittorrent.com:6969/announce",
+  "udp://tracker.torrent.eu.org:451/announce",
+  "udp://exodus.desync.com:6969/announce",
+  "udp://open.stealth.si:80/announce",
+];
+
+function buildMagnet(infoHash, name) {
+  const dn = encodeURIComponent(name);
+  const tr = TRACKERS.map((t) => `&tr=${encodeURIComponent(t)}`).join("");
+  return `magnet:?xt=urn:btih:${infoHash}&dn=${dn}${tr}`;
+}
+
+function unescapeEntities(s) {
+  return s
+    .replace(/&#0?38;|&amp;/g, "&")
+    .replace(/&#8211;|&#8212;/g, "-")
+    .replace(/&#8217;|&#0?39;|&apos;/g, "'")
+    .replace(/&#8220;|&#8221;|&quot;/g, '"')
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&#160;|&nbsp;/g, " ")
+    .replace(/&#(\d+);/g, (_, n) => String.fromCodePoint(parseInt(n, 10)))
+    .replace(/&#x([0-9a-fA-F]+);/g, (_, h) => String.fromCodePoint(parseInt(h, 16)));
+}
+
+module.exports = { TRACKERS, buildMagnet, unescapeEntities };
